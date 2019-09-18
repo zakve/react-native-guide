85c82463ce684d185316e188758ec70d335106fa
Martin Zaklasnik <martinzaklasnik@Martins-MacBook-Pro.local>
2019-09-18 14:35:19 +0200

    Add image

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  let pic = {
+    uri: 'https://media.unisot.com/2018/08/38071612_1870705499639411_1924707641431425024_n.png'
+  };
   return (
     <View style={styles.container}>
+      <Image source={pic} style={{width: 267, height: 100}}/>
       <Text>Hello Martin!</Text>
     </View>
   );
