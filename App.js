09ad95568054496577d6ec261180b108c7729116
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-09-23 16:30:12 +0200

    Add Task management

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import BaseInput from './components/BaseInput';
 
 export default function App() {
+  const [enteredTask, setEnteredTask] = useState('');
+  const [task, setTask] = useState([]);
+
+  const taskInputHandler = (enteredTask) => {
+    setEnteredTask(enteredTask)
+  }
+
+  const addTaskHandler = () => {
+    setTask([...task, enteredTask])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Add task..." style={styles.input} />
-        <Button title="Add" />
+        <TextInput placeholder="Add task..." style={styles.input} onChangeText={taskInputHandler} value={enteredTask} />
+        <Button title="Add" onPress={addTaskHandler} />
+      </View>
+      <View>
+        {task.map((task) => { return <Text key={task}>{task}</Text> })}
       </View>
     </View>
   );
