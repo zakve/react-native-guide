44bffe3ae6a91e88e098d5c1728d039f4124e330
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-09-24 11:35:15 +0200

    Add delete function on list item

--- App.js
+++ App.js
@@ -10,11 +10,17 @@ export default function App() {
     setTask([...task, { id: Math.random().toString(), value: taskTile }])
   }
 
+  const removeTaskHandler = taskId => {
+    setTask(currentTask => {
+      return (currentTask.filter((task) => task.id !== taskId))
+    })
+  }
+
   return (
     <View style={styles.screen}>
       <TaskInput onAddTask={addTaskHandler} />
       <FlatList keyExtractor={(item, index) => item.id} data={task} renderItem={itemData => (
-        <TaskItem title={itemData.item.value} onDelete={console.log('fdsf')} />
+        <TaskItem title={itemData.item.value} id={itemData.item.id} onDelete={removeTaskHandler} />
       )} />
     </View>
   );
